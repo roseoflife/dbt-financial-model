bc10c5c705d17717bf8ce50ecab12e92761f20ea
Tech User <maryami@gmail.com>
2025-09-02 23:55:16 +0000

    commit

--- models/marts/reporting/customer_revenues.sql
+++ models/marts/reporting/customer_revenues.sql
@@ -3,7 +3,7 @@
 SELECT 
     DATE_TRUNC('month', transaction_date) AS revenue_month,
     customer_id,
-    SUM(daily_revenue) AS monthly_revenue,
+    SUM(total_revenue) AS monthly_revenue,
     SUM(transaction_count) AS monthly_transactions
 FROM {{ ref('int_net_revenues') }}
 GROUP BY 
